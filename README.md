ef70d129afef704cfa967f75bd70514272451ee3
yuta0306 <you-2001-3-6@ezweb.ne.jp>
2023-03-07 17:51:37 +0900

    use hook

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # wandb-model-observer
+
+You can observe weghts/gradients/outputs of modules in your model!
+
+It is extremely useful to discover where an error happens.
+
+## Usage
+
+```python
+from wandb_model_observer import ModelObserver  # import `ModelObserver`
+
+class MyModel(nn.Module):
+    ...
+
+
+model = MyModel()
+observer = ModelObserver(model, key="wandb api key", project="your project name", group="your group name in the project")  # ready for observing your model
+
+# training loop
+...
+
+# log weights/gradients/outputs on W&B
+observer.commit()
+
+```
+
+## Details
+
+`ModelObserver` can monitor weights/gradients of each module in the model when `.backward()` is called, also outputs of each module are monitored when `.forward` is called
--- wandb_model_observer/observer.py
+++ wandb_model_observer/observer.py
@@ -7,78 +7,106 @@ import wandb
 
 
 class ModelObserver:
-    def __init__(self, model: nn.Module, key: str = "") -> None:
+    def __init__(
+        self,
+        model: nn.Module,
+        key: str = "",
+        *args,
+        **kwargs,
+    ) -> None:
         self.key = key
         self.is_login = self.login(key=key)
-        self.run = self.init()
+        self.run = self.init(*args, **kwargs)
 
         for n, mod in model.named_modules():
-            if hasattr(mod, "weight"):
-                self._register(module=mod, name=n, type_="weight")(mod.forward)
-            if hasattr(mod, "bias"):
-                self._register(module=mod, name=n, type_="bias")(mod.forward)
-            self._register(module=mod, name=n, type_="output")(mod.forward)
+            self._register_weight(name=n, module=mod)
+            self._register_output(name=n, module=mod)
+        for n, param in model.named_parameters():
+            self._register_gradient(name=n, param=param)
 
-    def _register(self, module: nn.Module, name: str, type_: str):
-        def outer(func):
-            def inner(*args, **kwargs):
-                ret = func(*args, **kwargs)
-                if type_ == "weight":
-                    tensor = getattr(module, "weight")
+    def _register_gradient(
+        self,
+        name: str,
+        param: "torch.nn.Parameter",
+        graph_type: str = "distribution",
+    ) -> None:
+        if not isinstance(param, torch.autograd.Variable):
+            return
+
+        if not param.requires_grad:
+            return
+
+        def log(grad: torch.Tensor):
+            if graph_type == "distribution":
+                tensor = torch.flatten(grad).detach().cpu().numpy()
+                self.run.log(
+                    {f"gradients/{name}": wandb.Histogram(sequence=tensor)},
+                    commit=False,
+                )
+            else:
+                raise NotImplementedError
+
+        param.register_hook(log)
+
+    def _register_weight(
+        self,
+        name: str,
+        module: "torch.nn.Module",
+        graph_type: str = "distribution",
+    ) -> None:
+        monitor = []
+        if hasattr(module, "weight"):
+            monitor.append("weight")
+        if hasattr(module, "bias"):
+            monitor.append("bias")
+
+        def log(module, grad_in, grad_out, monitor=monitor):
+            for m in monitor:
+                if graph_type == "distribution":
+                    tensor = getattr(module, m)
                     tensor = torch.flatten(tensor).detach().cpu().numpy()
                     self.run.log(
-                        {f"{name}.weight": wandb.Histogram(sequence=tensor)},
+                        {f"parameters/{name}.{m}": wandb.Histogram(sequence=tensor)},
                         commit=False,
                     )
-                if type_ == "bias":
-                    tensor = getattr(module, "bias")
-                    tensor = torch.flatten(tensor).detach().cpu().numpy()
+                else:
+                    raise NotImplementedError
+
+        if len(monitor) > 0:
+            module.register_full_backward_hook(log)
+
+    def _register_output(
+        self,
+        name: str,
+        module: "torch.nn.Module",
+        graph_type: str = "distribution",
+    ) -> None:
+        def log(module, input, output):
+            outputs = {}
+            if isinstance(output, torch.Tensor):
+                outputs.update({name: output})
+            elif isinstance(output, dict):
+                outputs.update(output)
+            elif isinstance(output, Iterable):
+                outputs.update({f"{name}.{i}": output[i] for i in range(len(output))})
+
+            for k, v in outputs.items():
+                if graph_type == "distribution":
+                    tensor = torch.flatten(v).detach().cpu().numpy()
                     self.run.log(
-                        {f"{name}.bias": wandb.Histogram(sequence=tensor)}, commit=False
+                        {f"outputs/{k}": wandb.Histogram(sequence=tensor)},
+                        commit=False,
                     )
-                if type_ == "output":
-                    if isinstance(ret, torch.Tensor):
-                        tensor = torch.flatten(ret).detach().cpu().numpy()
-                        self.run.log(
-                            {f"{name}.output": wandb.Histogram(sequence=tensor)},
-                            commit=False,
-                        )
-                    elif isinstance(ret, dict):
-                        for k, v in ret.items():
-                            if isinstance(v, torch.Tensor):
-                                tensor = torch.flatten(v).detach().cpu().numpy()
-                                self.run.log(
-                                    {
-                                        f"{name}.{k}.output": wandb.Histogram(
-                                            sequence=tensor
-                                        )
-                                    },
-                                    commit=False,
-                                )
-                    elif isinstance(ret, Iterable):
-                        for k, v in enumerate(ret):
-                            if isinstance(v, torch.Tensor):
-                                tensor = torch.flatten(v).detach().cpu().numpy()
-                                self.run.log(
-                                    {
-                                        f"{name}.{k}.output": wandb.Histogram(
-                                            sequence=tensor
-                                        )
-                                    },
-                                    commit=False,
-                                )
-                return ret
+                else:
+                    raise NotImplementedError
 
-            module.forward = inner
-            return inner
-
-        return outer
+        module.register_forward_hook(log)
 
     def login(self, key: str) -> bool:
         return wandb.login(key=key)
 
     def init(self, *args, **kwargs):
-        return wandb.init()
+        return wandb.init(*args, **kwargs)
 
     def commit(self):
         self.run.log(data={}, commit=True)
